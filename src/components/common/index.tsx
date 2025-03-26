09a3be54176adb6fc4c4c6ecf931e41116f0bc51
Tai Trinh <tatai22@clc.fitus.edu.vn>
2025-03-27 01:14:35 +0700

    làm lại trang 404

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -4,3 +4,4 @@ export { default as PremiumBadge } from "./PremiumBadge";
 export { default as PluginCard } from "./PluginCard";
 export { default as SplitText } from "./SplitText";
 export { default as Waves } from "./Waves";
+export { default as FuzzyText } from "./FuzzyText";
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { FuzzyText, Button } from "../components/common";
+import { HomeIcon } from "@heroicons/react/24/outline";
 
 function NotFound() {
   return (
@@ -11,18 +12,27 @@ function NotFound() {
         />
       </article>
       <div className="w-full max-w-4xl mx-auto text-center pt-24 px-6 pb-12">
-        <div className="py-16">
-          <h1 className="text-9xl font-bold text-black mb-4">404</h1>
+        <div className="flex flex-col items-center justify-center py-16">
+          <div className="flex justify-center items-center mb-8">
+            <FuzzyText
+              baseIntensity={0.2}
+              hoverIntensity={1}
+              enableHover={true}
+              color="black"
+            >
+              404
+            </FuzzyText>
+          </div>
           <h2 className="text-3xl font-semibold mb-6">Page Not Found</h2>
           <p className="text-xl text-gray-600 mb-10 max-w-lg mx-auto">
             The page you are looking for doesn't exist or has been moved.
           </p>
-          <Link
-            to="/"
-            className="px-6 py-3 bg-black text-white rounded-md hover:bg-gray-800 transition-colors inline-block"
-          >
-            Go Back Home
-          </Link>
+          <Button to="/" className="w-fit" variant="primary">
+            <div className="flex justify-center items-center w-full gap-2 group-hover:gap-4 transition-all duration-50">
+              <HomeIcon className="w-5 h-5" />
+              <span>Go Back Home</span>
+            </div>
+          </Button>
         </div>
       </div>
     </>
